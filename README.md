a6ed06cba1fadd7fe9abd4e11dd7dbabc21835b3
泠风寒声 <lfhsheng@outlook.com>
2022-08-24 20:05:03 +0800

    添加代码与说明文档

---
+++ README.md
@@ -0,0 +1,5 @@
+# 关于此~~废物~~机器人
+此机器人使用[go-cqhttp](https://docs.go-cqhttp.org)，
+
+`http`端口：`5700`
+`websocket`端口：`5701`
---
+++ main.py
@@ -0,0 +1,94 @@
+import websocket
+import json
+import rel
+import requests
+import os
+from datetime import datetime
+replyList = [
+    ["有人吗", "没人"],
+    ["傻逼", "智障"],
+    ["在吗", "不在"],
+    ["qwq", "qaq"],
+    ["awa", "quq"],
+]
+jsonPath = "./checkin.json"
+if not os.path.exists(jsonPath):
+    initDict = {"user": [], "name": [], "count": [], "day": []}
+    initJson = json.dumps(initDict)
+    print("正在生成用于存储签到次数的json")
+    openInitJson = open(jsonPath, "w")
+    openInitJson.write(initJson)
+    openInitJson.close()
+def sendPrivateMessage(id, message):
+    requests.get(
+        "http://127.0.0.1:5700/send_msg?user_id=%d&message=%s" % (id, message))
+def sendGroupMessage(id, message):
+    requests.get(
+        "http://127.0.0.1:5700/send_group_msg?group_id=%d&message=%s" % (id, message))
+def getMessage(ws, message):
+    m = dict(json.loads(message))
+    #if m['post_type'] == 'heartbreat' or m["post_type"] == "meta_event":
+    #    return None
+    if m["message"] == ".签到" or m["message"] == ".check in":
+        print("有人在签到！")
+        readJson = open(jsonPath, "r")
+        jsonDict = json.load(readJson)
+        userList = jsonDict["user"]
+        nameList = jsonDict["name"]
+        countList = jsonDict["count"]
+        dayList = jsonDict["day"]
+        try:
+            day = dayList[0]
+        except:
+            dayList.append(datetime.today().day)
+            print("添加日期")
+        try:
+            index = userList.index(m["user_id"])
+            if dayList[0] != datetime.today().day:
+                dayList.clear()
+                dayList.append(datetime.today().day)
+                countList[index] += 1
+                createJson = {"user": userList, "name": nameList,
+                              "count": countList, "day": dayList}
+                newJson = json.dumps(createJson)
+                openJson = open(jsonPath, "w")
+                openJson.write(newJson)
+                openJson.close()
+            if m["message_type"] == "private":
+                sendPrivateMessage(m["user_id"], "%s签到了%d天" % (
+                    m["sender"]["nickname"], countList[index]))
+        except:
+            userList.append(m["user_id"])
+            countList.append(1)
+            nameList.append(m["sender"]["nickname"])
+            createJson = {"user": userList, "name": nameList,
+                          "count": countList, "day": dayList}
+            newJson = json.dumps(createJson)
+            openJson = open(jsonPath, "w")
+            openJson.write(newJson)
+            openJson.close()
+            print("not")
+            if m["message_type"] == "private":
+                print("ok")
+                sendPrivateMessage(m["user_id"], "%s签到了%d天" % (m["sender"]["nickname"],1))
+        return None
+    if m["post_type"] == "message":
+        flag = False
+        for index in range(0, len(replyList)):
+            if m["message"] in replyList[index][0]:
+                flag = True
+                if m["message_type"] == "private":
+                    sendPrivateMessage(m["user_id"], replyList[index][1])
+                if m["message_type"] == "group":
+                    sendGroupMessage(m["group_id"], replyList[index][1])
+        '''
+        if flag == False:
+            if m["message_type"] == "private":
+                sendPrivateMessage(m["user_id"], "啊吧啊吧")
+        '''
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://127.0.0.1:5701/", on_message=getMessage)
+    ws.run_forever(dispatcher=rel)
+    rel.signal(2, rel.abort)
+    rel.dispatch()
